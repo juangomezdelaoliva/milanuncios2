4e2377df2a9da8694c607d7b7abde99c46e537ba
unknown <juan.gomez.freelance@gmail.com>
2023-03-30 16:35:56 +0200

    first commit

---
+++ src/main/java/com/example/milanuncios2/Milanuncios2Application.java
@@ -0,0 +1,13 @@
+package com.example.milanuncios2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milanuncios2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Milanuncios2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/milanuncios2/Milanuncios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milanuncios2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Milanuncios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
